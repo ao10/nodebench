abf9168b6d5ead3cf8859640e4f16b3e8960099a
ao10 <ao10@umbc.edu>
2020-05-28 17:28:26 -0400

    Compare Object.fromEntries vs _.transform

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const Benchmark = require('benchmark');
+const _ = require('lodash');
 
 const suite = new Benchmark.Suite();
 
+const obj = { 'Content-Type': 'application/pdf' };
+
 suite
-  .add('A', () => {
-    const str = 'B';
+  .add('Object.fromEntries', () => {
+    const newObj = Object.fromEntries(
+      Object.entries(obj).map(([k, v]) => [k.toLowerCase(), v]),
+    );
+
+    console.log(newObj);
   })
-  .add('B', () => {
-    const str = 'B';
+  .add('_.transform', () => {
+    const lowerObj = _.transform(obj, function (result, val, key) {
+      result[key.toLowerCase()] = val;
+    });
+    console.log(lowerObj);
   })
   .on('cycle', function onCycle(event) {
     console.log(event.target.toString());
